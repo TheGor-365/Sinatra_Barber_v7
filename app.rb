d5d85d83d36f025c03cca88839b0bcf3f70c1e1d
Gor <7495500@gmail.com>
2020-04-17 12:24:38 +0300

    Initial commit

---
+++ app.rb
@@ -0,0 +1,32 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
+end
+
+get '/about' do
+
+	erb :about
+end
+
+get '/visit' do
+
+	erb :visit
+end
+
+get '/contacts' do
+
+	erb :contacts
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@date_time = params[:date_time]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	erb :visit
+end
